67e4eaebf1d24f2681058b34eba35ea5354badd6
Muhammed Tariq <momotrq94@gmail.com>
2022-04-06 16:51:06 -0400

    added findKeyByValue

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(Obj, val) {
+  let keyArray = Object.keys(Obj);
+  for (const key of keyArray) {
+    if (Obj[key] === val) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
