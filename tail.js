7d47ce054254eff7a525070af44043898003f263
Muhammed Tariq <momotrq94@gmail.com>
2022-04-11 15:38:11 -0400

    dried tail.js and added its tests

--- tail.js
+++ tail.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢🟢🟢Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(array1) {
   let outputArray;
   if (array1.length === 0 || array1.length === 1) {
@@ -16,11 +8,4 @@ const tail = function(array1) {
   return outputArray;
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
-
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
